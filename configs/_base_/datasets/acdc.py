27f3c8c1b5ada4d9f6bdcbff8b9f1c6ec2862bc1
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-04-16 14:47:31 +0800

    update 22

--- configs/_base_/datasets/acdc.py
+++ configs/_base_/datasets/acdc.py
@@ -41,7 +41,7 @@ tta_pipeline = [
         ])
 ]
 train_dataloader = dict(
-    batch_size=32,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
@@ -52,7 +52,7 @@ train_dataloader = dict(
             img_path='img_dir/train', seg_map_path='ann_dir_0/train'),
         pipeline=train_pipeline))
 val_dataloader = dict(
-    batch_size=16,
+    batch_size=1,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
