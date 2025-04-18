675a19676253b0f4d9014c343a9962893fa2ce8f
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-04-18 12:52:28 +0800

    update 29

--- configs/acdc/unet-s5-d16_pspnet_4xb4-40k_acdc-64x64.py
+++ configs/acdc/unet-s5-d16_pspnet_4xb4-40k_acdc-64x64.py
@@ -2,8 +2,21 @@ _base_ = [
     '../_base_/models/pspnet_unet_s5-d16.py', '../_base_/datasets/acdc.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
-crop_size = (64, 64)
+# crop_size = (64, 64)
+# data_preprocessor = dict(size=crop_size)
+# model = dict(
+#     data_preprocessor=data_preprocessor,
+#     test_cfg=dict(crop_size=(64, 64), stride=(42, 42)))
+
+crop_size = (256, 256)
 data_preprocessor = dict(size=crop_size)
 model = dict(
     data_preprocessor=data_preprocessor,
-    test_cfg=dict(crop_size=(64, 64), stride=(42, 42)))
+    decode_head=dict(num_classes=3),
+    auxiliary_head=dict(num_classes=3),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+train_dataloader = dict(batch_size=16, num_workers=4)
+val_dataloader = dict(batch_size=8, num_workers=4)
+test_dataloader = val_dataloader
