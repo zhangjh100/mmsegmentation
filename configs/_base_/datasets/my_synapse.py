7b92d9c80e0ee2916da70115d9d4a3f5ec1de1ca
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-04-22 10:56:41 +0800

    update 35

--- configs/_base_/datasets/my_synapse.py
+++ configs/_base_/datasets/my_synapse.py
@@ -12,8 +12,8 @@ train_pipeline = [
         ratio_range=(0.5, 2.0),
         keep_ratio=True),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0.5),
-    dict(type='PhotoMetricDistortion'),
+    # dict(type='RandomFlip', prob=0.5),
+    # dict(type='PhotoMetricDistortion'),
     dict(type='PackSegInputs')
 ]
 test_pipeline = [
@@ -59,7 +59,7 @@ val_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        data_prefix=dict(img_path='img_dir/val', seg_map_path='ann_dir/val'),
+        data_prefix=dict(img_path='img_dir/test', seg_map_path='ann_dir/test'),
         pipeline=test_pipeline))
 test_dataloader = val_dataloader
 
